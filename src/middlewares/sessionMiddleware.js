3109e58259928b727e794b0f8ee5c68b57a39137
Alain Bonneau <alaindev97@gmail.com>
2024-06-26 22:16:27 +0200

    configure database

--- src/middlewares/sessionMiddleware.js
+++ src/middlewares/sessionMiddleware.js
@@ -1,23 +1,27 @@
-const pg = require("pg");
+const { Pool } = require("pg");
 const session = require("express-session");
 const pgSession = require("connect-pg-simple")(session);
 
 // Connexion à la base de données
-const client = new pg.Client(process.env.PG_URL);
-client.connect();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 // Middleware de session
 const sessionMiddleware = session({
-  store: new pgSession({ // Store de session
-    pool: client, // Pool de connexion à la base de données
-    tableName: "user_session", // Nom de la table de session
-    createTableIfMissing: true, // Crée la table de session si elle n'existe pas
+  store: new pgSession({ 
+    pool: pool, // Pool de connexion à la base de données
+    tableName: "user_session", 
+    createTableIfMissing: true, 
   }),
 
-  secret: process.env.SESSION_SECRET, // Clé secrète pour signer le cookie de session
-  saveUninitialized: true, // Sauvegarde la session même si elle n'est pas modifiée
-  resave: false, // Ne sauvegarde pas la session si elle n'a pas été modifiée
-  cookie: { secure: false }, // Cookie non sécurisé (http)
+  secret: process.env.SESSION_SECRET, 
+  saveUninitialized: true, 
+  resave: false, 
+  cookie: { secure: false }, 
 });
 
 module.exports = sessionMiddleware;
