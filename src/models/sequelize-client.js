730f17552ecf71b832790945c856edfdf1afd916
Alain Bonneau <alaindev97@gmail.com>
2024-06-26 23:11:49 +0200

    test

--- src/models/sequelize-client.js
+++ src/models/sequelize-client.js
@@ -3,7 +3,7 @@ require("dotenv/config");
 const { Sequelize } = require("sequelize");
 
 // Créer une instance de connexion à notre BDD oquiz
-const sequelize = new Sequelize(process.env.PG_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: console.log,
   define: {
     createdAt: "created_at",
