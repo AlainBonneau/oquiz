440e923e75035af68528e2473ef082f042785ab9
Alain Bonneau <alaindev97@gmail.com>
2024-06-27 02:23:21 +0200

    dfvdf

--- src/models/sequelize-client.js
+++ src/models/sequelize-client.js
@@ -17,4 +17,13 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
+
 module.exports = sequelize;
