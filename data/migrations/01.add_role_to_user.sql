77eaf05ac63913168aac531e6a187467df0481d7
Alain Bonneau <alaindev97@gmail.com>
2024-06-27 03:00:09 +0200

    ffff

--- data/migrations/01.add_role_to_user.sql
+++ data/migrations/01.add_role_to_user.sql
@@ -1,10 +1,9 @@
 -- Script de migration pour modifier la table user et ajouter le champ role !
 
-BEGIN;
+
 
 DROP TABLE IF EXISTS role_name;
 CREATE TYPE role_name AS ENUM ('member', 'admin');
 
 ALTER TABLE "user" ADD COLUMN "role" role_name NOT NULL DEFAULT 'member';
 
-COMMIT;
